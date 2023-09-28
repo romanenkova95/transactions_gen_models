4b20b4dae02ee60822003d27ffb135ff321a8a67
abazarova <bazarova.ai@phystech.edu>
2023-09-28 18:12:17 +0530

    add timecl sampler

--- src/coles/datamodule.py
+++ src/coles/datamodule.py
@@ -1,20 +1,18 @@
 """
 Custom coles datamodule
 """
-import numpy as np
-
-import torch
-
-from typing import Optional, List, Dict, Tuple
 from functools import reduce
 from operator import iadd
+from typing import Dict, List, Optional, Tuple
 
+import numpy as np
+import torch
 from ptls.data_load.datasets import MemoryMapDataset
 from ptls.data_load.iterable_processing import SeqLenFilter
-from ptls.frames.coles import ColesDataset
-from ptls.frames.coles.split_strategy import SampleSlices, AbsSplit
-from ptls.data_load.utils import collate_feature_dict
 from ptls.data_load.padded_batch import PaddedBatch
+from ptls.data_load.utils import collate_feature_dict
+from ptls.frames.coles import ColesDataset
+from ptls.frames.coles.split_strategy import AbsSplit, SampleSlices
 
 
 class SampleAll(AbsSplit):
@@ -32,7 +30,7 @@ class SampleAll(AbsSplit):
         self.seq_len = seq_len
         self.stride = stride
 
-    def split(self, dates: np.array) -> List[np.array]:
+    def split(self, dates: np.ndarray) -> List[np.ndarray]:
         """Create list of subsequences indexes.
 
         Args:
@@ -51,6 +49,79 @@ class SampleAll(AbsSplit):
         # crop last 'seq_len' record as we do not have local labels for them
         start_pos = date_range[0 : date_len - self.seq_len : self.stride]
         return [date_range[s : s + self.seq_len] for s in start_pos]
+
+
+class TimeCLSampler(AbsSplit):
+    """
+        TimeCL sampler implementation, ptls-style.
+        For details, see Algorithm 1 from the paper:
+            http://mesl.ucsd.edu/pubs/Ranak_AAAI2023_PrimeNet.pdf
+        Args:
+            min_len (int): minimum subswequence length
+            max_len (int): maximum subsequence lentgh
+            llambda (float): lower bound for lambda valu
+            rlambda (float): upper bound for lambda value
+            split_count (int): number of generated subsequences
+    """
+    def __init__(
+        self,
+        min_len: int,
+        max_len: int,
+        llambda: float,
+        rlambda: float,
+        split_count: int,
+    ) -> None:
+        self.min_len = min_len
+        self.max_len = max_len
+        self.llambda = llambda
+        self.rlambda = rlambda
+        self.split_count = split_count
+
+    def split(self, dates: np.ndarray) -> List[list]:
+        """Create list of subsequences indexes.
+
+        Args:
+            dates (np.array): array of timestamps with transactions datetimes
+
+        Returns:
+            list(np.arrays): list of indexes, corresponding to subsequences
+        """
+        date_len = dates.shape[0]
+        idxs = np.arange(date_len)
+        if date_len <= self.min_len:
+            return [idxs for _ in range(self.split_count)]
+
+        time_deltas = np.concatenate(
+            (
+                [dates[1] - dates[0]],
+                0.5 * (dates[2:] - dates[:-2]),
+                [dates[-1] - dates[-2]],
+            )
+        )
+
+        idxs = sorted(idxs, key=lambda idx: time_deltas[idx])
+
+        dense_timestamps, sparse_timestamps = (
+            idxs[: date_len // 2],
+            idxs[date_len // 2 :],
+        )
+
+        max_len = date_len if date_len < self.max_len else self.max_len
+
+        lengths = np.random.randint(self.min_len, max_len, size=self.split_count)
+        lambdas = np.random.uniform(self.llambda, self.rlambda, size=self.split_count)
+
+        n_dense, n_sparse = np.floor(lengths * lambdas).astype(int), np.ceil(
+            lengths * (1 - lambdas)
+        ).astype(int)
+
+        idxs = [
+            list(np.random.choice(dense_timestamps, size=n_d, replace=False)) +
+            list(np.random.choice(sparse_timestamps, size=n_s, replace=False))
+            for (n_d, n_s) in list(zip(n_dense, n_sparse))
+        ]
+
+        return [sorted(idx) for idx in idxs]
 
 
 class CustomColesDataset(ColesDataset):
@@ -154,3 +225,39 @@ class CustomColesValidationDataset(ColesDataset):
                 i for i, class_samples in enumerate(batch) for _ in class_samples
             ]
             return padded_batch, torch.LongTensor(class_labels)
+
+
+class ColesTimeCLDataset(ColesDataset):
+    """
+    CoLES dataset with the sampler from the paper 
+        http://mesl.ucsd.edu/pubs/Ranak_AAAI2023_PrimeNet.pdf
+    
+    Args:
+        data (list[dict]): transaction dataframe in the ptls format (list of dicts)
+        min_len (int): minimum subswequence length
+        max_len (int): maximum subsequence lentgh
+        llambda (float): lower bound for lambda valu
+        rlambda (float): upper bound for lambda value
+        split_count (int): number of generated subsequences
+        col_time (str, optional): column name with event time. Defaults to 'event_time'.
+    """
+
+    def __init__(
+        self,
+        data: List[Dict[str, torch.Tensor]],
+        min_len: int,
+        max_len: int,
+        llambda: float,
+        rlambda: float,
+        split_count: int,
+        *args,
+        col_time: str = "event_time",
+        **kwargs
+    ):
+        super().__init__(
+            MemoryMapDataset(data, [SeqLenFilter(min_len)]),
+            TimeCLSampler(min_len, max_len, llambda, rlambda, split_count),
+            col_time,
+            *args,
+            **kwargs
+        )
