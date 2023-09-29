1675cd39a34f187a9c3affa4ed19f01fb9665115
i.kuleshov <i.kuleshov@skoltech.edu>
2023-09-29 14:18:31 +0000

    minor fixes

--- src/generation/learning/ae_learning.py
+++ src/generation/learning/ae_learning.py
@@ -15,18 +15,19 @@ from src.preprocessing import preprocess
 
 logger = get_logger(name=__name__)
 
+
 def train_autoencoder(
     cfg_preprop: DictConfig, cfg_dataset: DictConfig, cfg_model: DictConfig
 ) -> None:
     """Train autoencoder, specified in the model config, on the data, specified by the preprocessing config.
-     
+
     Args:
-        cfg_preprop (DictConfig): 
+        cfg_preprop (DictConfig):
             the preprocessing config, for specifications see preprocess.
-        cfg_dataset (DictConfig): 
+        cfg_dataset (DictConfig):
             the dataset config, to be partially instantiated, and later supplied the data argument,
             a list of dicts of features, outputted by preprocess.
-        cfg_model (DictConfig): 
+        cfg_model (DictConfig):
             the model config, which should contain:
                 - encoder: nn.Module of encoder
                 - decoder: nn.Module of decoder
@@ -35,19 +36,18 @@ def train_autoencoder(
                 - datamodule_args: arguments to pass when constructing the ptls datamodule. Optional, defaults to {}
                 - split_seed: randomness seed to use when splitting records. Optional, defaults to 42
     """
-    data = preprocess(OmegaConf.to_container(cfg_preprop)) # type: ignore
+    data = preprocess(OmegaConf.to_container(cfg_preprop))  # type: ignore
 
     train, valid = train_test_split(data, test_size=cfg_model["test_size"])
     ds_factory = instantiate(cfg_dataset, _partial_=True)
     datamodule = PtlsDataModule(
         train_data=ds_factory(train),
         valid_data=ds_factory(valid),
-        **cfg_model.get("datamodule_args", {})
+        **cfg_model.get("datamodule_args", {}),
     )
 
     module: AbsAE = instantiate(cfg_model["module_ae"], _recursive_=False)(
-        encoder_config=cfg_model["encoder"],
-        decoder_config=cfg_model["decoder"]
+        encoder_config=cfg_model["encoder"], decoder_config=cfg_model["decoder"]
     )
 
     callbacks = []
@@ -55,9 +55,13 @@ def train_autoencoder(
     if "fast_dev_run" not in cfg_model["trainer_args"]:
         lightning_logger = WandbLogger(project="macro_micro_coles")
 
-        cfg = OmegaConf.merge(cfg_model, cfg_preprop)
-        lightning_logger.experiment.config.update(OmegaConf.to_container(cfg))
-        
+        cfg_dict = {
+            "preprocessing": OmegaConf.to_container(cfg_preprop),
+            "dataset": OmegaConf.to_container(cfg_dataset),
+            "model": OmegaConf.to_container(cfg_model)
+        }
+
+        lightning_logger.experiment.config.update(cfg_dict)
         callbacks.append(LearningRateMonitor())
     else:
         lightning_logger = DummyLogger()
