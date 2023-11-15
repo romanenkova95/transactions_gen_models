a7673aa163d55791dc54db2863d67c08f08a2191
abazarova <a.bazarova@ira-labs.com>
2023-11-15 14:19:24 +0000

    upd learn_coles

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO)
 @hydra.main(version_base=None, config_path="config", config_name="config_churn")
 def main(cfg: Optional[DictConfig] = None) -> None:
     if cfg["model"]["name"].startswith("coles"):
-        learn_coles(cfg["dataset"], cfg["model"])
+        learn_coles(cfg["preprocessing"], cfg["dataset"], cfg["model"])
         # res = global_target_validation(cfg["dataset"], cfg["validation"])
         # res = local_target_validation(cfg["dataset"], cfg["validation"])
     elif cfg["model"]["name"].startswith("cpc"):
