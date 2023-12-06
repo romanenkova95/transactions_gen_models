525858e19dcf0d0905ff95bbafea2d81d19d86f6
i.kuleshov <i.kuleshov@skoltech.edu>
2023-12-06 18:35:28 +0000

    restore working version of log cosh loss

--- src/losses/log_cosh_loss.py
+++ src/losses/log_cosh_loss.py
@@ -3,43 +3,26 @@ import math
 
 
 class LogCoshLoss(torch.nn.Module):
-    """Log-cosh loss for return time prediction head training, approximates MAE"""
+    """Custom LogCoshLoss for return time prediction head training."""
+
     def __init__(self, reduction: str = "mean") -> None:
-        """Initialize LogCoshLoss.
-        
-        Args:
-            reduction (str) - type of loss reduction ('mean' or 'sum')
-        """
         super().__init__()
         if reduction not in ["mean", "sum"]:
             raise ValueError("LogCoshLoss: reduction not in ['mean', 'sum']")
-        self.__reduction = reduction
+        self.reduction = reduction
 
     @staticmethod
-    def __log_cosh(x: torch.Tensor) -> torch.Tensor:
-        """Compute LogCosh function.
-        
-        Args:
-            x (torch.Tensor) - input values
-        
-        Returns:
-            LogCosh(x) for |x| < 10, and |x| elsewhere
-        """
-        out = x + torch.nn.functional.softplus(-2.0 * x) - math.log(2.0)
-        out[torch.abs(x) > 10] = torch.abs(x[torch.abs(x) > 10])
-        return out
+    def _log_cosh(x: torch.Tensor) -> torch.Tensor:
+        """Compute LogCosh function."""
+        return torch.where(
+            torch.abs(x) > 10,
+            torch.abs(x),
+            x + torch.nn.functional.softplus(-2.0 * x) - math.log(2.0)
+        )
 
     def forward(self, y_true: torch.Tensor, y_pred: torch.Tensor) -> torch.Tensor:
-        """Compute loss, apply reduction.
-        
-        Args:
-            y_true (torch.Tensor) - true return times
-            y_pred (torch.Tensor) - predicted return times
-        
-        Returns:
-            value of the loss function for a batch
-        """
-        if self.__reduction == "mean":
-            return torch.mean(self.__log_cosh(y_pred - y_true))
+        """Compute loss."""
+        if self.reduction == "mean":
+            return torch.mean(self._log_cosh(y_pred - y_true))
         else:
-            return torch.sum(self.__log_cosh(y_pred - y_true))
+            return torch.sum(self._log_cosh(y_pred - y_true))
