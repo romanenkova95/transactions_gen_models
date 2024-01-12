e68e4d9b6116b807a0ca72edf81f7707870c6da8
Alexander Stepikin <st.alexandr_2902@mail.ru>
2024-01-12 17:16:21 +0000

    fix __init__ files

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,4 @@
 """Preprocessing init module, for easier preprocessing specification in configs."""
-
 from .category_transforms import DropRare
 from .datetime_transforms import CustomDatetimeNormalization, DropDuplicates
 from .numeric_transforms import DropQuantile, ToType
