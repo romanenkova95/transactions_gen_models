809e3305e2d03771222709129bc2911cb8e47cdb
Alexander <44891804+stalex2902@users.noreply.github.com>
2024-03-19 23:03:41 +0300

    Clean-up

--- src/local_validation/__init__.py
+++ src/local_validation/__init__.py
@@ -1,4 +1,4 @@
 """Init module."""
 
-from .binary import BinaryLocalVal  # noqa: F401
-from .categorical import CategoricalLocalVal  # noqa: F401
+from .binary import BinaryLocalVal
+from .categorical import CategoricalLocalVal
